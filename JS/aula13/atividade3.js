24dfcd3f4d2523876264142546fe06017298a9ad
John-marleson <johnmarlesom67@gmail.com>
2026-03-19 15:43:33 -0300

    aula digital college

---
+++ JS/aula13/atividade3.js
@@ -0,0 +1,8 @@
+let nota1 = Number(prompt("digite a nota 1: "))
+let nota2 = Number(prompt("digite a nota 2: "))
+
+if(nota1 > nota2){
+    console.log(nota1 + " é maior que " + nota2 + "!")
+}else if(nota1 < nota2){
+    console.log(nota1 + " é menor que " + nota2 + "!")
+}
